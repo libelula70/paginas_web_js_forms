e65c9bc4da42f7ef61f5c17e3333de9aedbb8b95
libelula70 <anelen70@gmail.com>
2018-09-04 13:59:11 +0200

    interactivo

---
+++ 04_Interactivo/loader.js
@@ -0,0 +1,12 @@
+function app () {
+    if ()
+    document.querySelector ('#mas').checked
+}
+
+for (let i = 0; i < aTurnos.length; i++) {
+    const item = aTurnos[i];
+    if (item.checked) {
+        return item.value
+
+
+document.addEventListener ('DOMContentLoaded', app)
