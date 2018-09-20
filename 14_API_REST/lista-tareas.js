25b7d5518a6a4a6b8b63ff7e1bbbf6e8a657a08a
libelula70 <anelen70@gmail.com>
2018-09-20 12:30:21 +0200

    api rest final

--- 14_API_REST/lista-tareas.js
+++ 14_API_REST/lista-tareas.js
@@ -5,7 +5,9 @@ export class ListaTareas {
         this.nodoListaTareas = document.querySelector('#lista')
         this.nodoBtnAdd = document.querySelector('#btnAdd')
         this.nodoNewTarea = document.querySelector('#tarea')
+        this.nodoBtnBorrarSelect = document.querySelector('#btn-borrar-select')
         this.nodoBtnAdd.addEventListener('click', this.addTarea.bind(this))
+        this.nodoBtnBorrarSelect.addEventListener('click', this.borrarSelect.bind(this))
         this.uRL = 'http://localhost:3000/tareas'
         this.aTareas = []
         this.fetchServices = new FetchService ()
@@ -16,6 +18,7 @@ export class ListaTareas {
         this.fetchServices.send(this.uRL, {method: 'GET' })
             .then( data => {
                 this.aTareas = data
+                console.log("Ejecutando getTareas")
                 this.renderLista()
             },
             error => {console.dir(error)}
@@ -23,9 +26,18 @@ export class ListaTareas {
     }
     
     renderLista() {
+        this.nodoListaTareas.innerHTML=''
         let html = ''
+        this.nodoBtnBorrarSelect.disabled = true
+
         this.aTareas.forEach(
-            item =>  html += this.renderTarea(item)
+            item =>  {
+                if (item.isComplete && this.nodoBtnBorrarSelect.disabled) {
+                    this.nodoBtnBorrarSelect.disabled = false
+                }
+                html += this.renderTarea(item)
+
+            }
         )
         this.nodoListaTareas.innerHTML = html
         this.aNodosChecks = document.querySelectorAll('[name="is-completa"]')
@@ -91,16 +103,41 @@ export class ListaTareas {
         )
     }
 
-    borrarTarea(oEv) {
-        if (!window.confirm('Seguro')) {return}
-        //TODO borrar en el servicio web
-        let url= this.uRL + '/' + oEv.target.dataset.id
+    borrarTarea(p) {
+        let id 
+        
+        if (p.target) {
+            id = p.target.dataset.id
+            if (!window.confirm('Seguro')) {return}
+        } else {id = p.id}
+
+        console.log(id)
+            //borrar en el servicio web
+        let url= this.uRL + '/' + id
         this.fetchServices.send(url, {method: 'DELETE'})
         .then(
-            data => this.getTareas(),
+            data => {
+                if (p.target || p.isUltima) {
+            this.getTareas()}
+        },
             error => console.log(error)
         )
         
     }
+    borrarSelect() {
+        let aSeleccionados = this.aTareas.filter(
+            (item) => {return item.isComplete}
+        )
+        // Si no controlamos el disabled del botón
+        // if(!aSeleccionados.length) {return}
 
+        if (!window.confirm('Seguro. Vas a borrar todos los registros')) {return}
+        
+        aSeleccionados.forEach(
+            (item, i, array) => {
+                let isUltima = (i+1 === array.length) ? true : false
+                this.borrarTarea({id: item.id, isUltima: isUltima})
+            }
+        )
+    }
 }
