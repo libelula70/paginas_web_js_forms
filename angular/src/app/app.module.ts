53583c503fe9a74305f94f9519add5ad043518f2
libelula70 <anelen70@gmail.com>
2018-09-26 18:51:06 +0200

    cambios del css

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
