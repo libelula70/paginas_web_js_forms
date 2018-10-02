db17799fbedc4d4cb6ec178cc01de727aa42cf39
libelula70 <anelen70@gmail.com>
2018-10-02 13:46:31 +0200

    tareas plus

---
+++ angular/src/app/models/tarea.model.ts
@@ -0,0 +1,18 @@
+export interface TareaIf {
+  nombre: string
+  isCompleted: boolean
+}
+
+
+export class TareaModel implements TareaIf {
+  /*   nombre: string
+    constructor(nombre) {
+      this.nombre= nombre
+    } */
+  
+  constructor(
+    public nombre: string,
+    public isCompleted: boolean
+    ) {}
+}
+
--- angular/src/app/tareas/tareas-plus/tareas-plus.component.ts
+++ angular/src/app/tareas/tareas-plus/tareas-plus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TareaModel, TareaIf } from '../../models/tarea.model';
 
 @Component({
   selector: 'cas-tareas-plus',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasPlusComponent implements OnInit {
 
+  titulo: string
+  //newTarea: TareaModel
+  newTarea: TareaIf
+  aTareas: Array<TareaIf>
+  
   constructor() { }
 
   ngOnInit() {
+    this.titulo = 'Lista Plus'
+    //this.newTarea = new TareaModel('', false)
+    this.newTarea = {nombre:'', isCompleted:false}
+    this.aTareas = []
   }
-
+  addTarea() {
+    if (!this.newTarea.nombre) {return}
+    //let tareaClon = JSON.parse(JSON.stringify(this.newTarea))
+    let tareaClon = Object.assign({}, this.newTarea)
+    this.aTareas.push(tareaClon)
+    console.log(this.aTareas)
+    this.newTarea.nombre=''
+  }
+  completarTarea(i) {
+    console.log(this.aTareas)
+  }
+  borrarTarea(i) {
+    this.aTareas.splice(i,1)
+  }
 }
